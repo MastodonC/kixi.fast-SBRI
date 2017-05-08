2ab6e32596081361a0d22754ca971c67b3327e68
Elise Huard <elise@mastodonc.com>
2017-05-08 14:55:25 +0100

    total admissions predictions to scale other prediction levels

--- data-exploration/patient-admissions.R
+++ data-exploration/patient-admissions.R
@@ -2,6 +2,7 @@ require(dplyr)
 require(reshape2)
 require(ggplot2)
 require(lubridate)
+require(forecast)
 
 # for our own data source paths
 source("paths.R")
@@ -10,6 +11,12 @@ all_na_to_0 <- function(d) {
   d[is.na(d)] <- 0
   d
 }
+
+remove_first_and_last_n <- function(dataframe, n) {
+  head(dataframe[-n,], -n) # remove first and last row
+}
+
+prediction.length <- 20
 
 patient.data <- read.csv(patient.data.path, stringsAsFactors = F, na.strings=c("","NA")) %>%
   mutate(
@@ -58,7 +65,6 @@ acf(patients.per.day$medical) # 7 day cycle
 acf(patients.per.day$palliative) # 7 day cycle
 acf(patients.per.day$surgical)
 pacf(patients.per.day$surgical) # possible 7 day cycle, however memory effect
-acf(patients.per.day$unscheduled)
 acf(patients.per.day$women.child) # might be 7 day cycle
 
 patients.per.week <- tally(group_by(patient.admissions, Year.Week, Resource.Pool.name)) %>%
@@ -82,13 +88,49 @@ ggplot(patients.per.week, aes(Year.Week)) +
   geom_line(aes(y = patients.per.week$medical, colour = "Medical", group=1))
 acf(patients.per.week$medical)
 
+# predict totals
+total.patients.per.week <- group_by(patient.admissions, Year.of.Admission, Week.of.Admission) %>%
+                           summarize(weekly.count = n()) %>%
+                           remove_first_and_last_n(1)
+
+last.year <- max(total.patients.per.week$Year.of.Admission)
+last.week <- as.integer(last(total.patients.per.week$Week.of.Admission))
+
+total.patients.model <- arima(total.patients.per.week$weekly.count, order = c(1,0,1))
+plot.ts(total.patients.per.week$weekly.count)
+total.prediction <- predict(total.patients.model, n.ahead = prediction.length)
+total.prediction.data <- mutate(data.frame(Year.of.Admission = rep(last.year,prediction.length),
+                                           Week.of.Admission = (last.week+1):(last.week+prediction.length),
+                                           weekly.count = total.prediction$pred[1:prediction.length]),
+                         weekly.count = as.integer(weekly.count),
+                         Week.of.Admission = sprintf("%02d", Week.of.Admission),
+                         Year.of.Admission = as.character(Year.of.Admission))
+total.admissions <- bind_rows(total.patients.per.week, total.prediction.data)
+
+
 # check resource pool per type of admission
 # emergency admissions
-emergency.patients.per.week <- group_by(patient.admissions, Year.Week, Resource.Pool.name) %>%
-                     filter(Method.of.Admission.Category == "Emergency Admission") %>%
-                     tally() %>%
-                     dcast(Year.Week ~ Resource.Pool.name)  %>%
-                     setNames(c("Year.Week","elderly","medical","palliative","surgical","unscheduled","women.child")) %>%
-                     all_na_to_0()
-# pretty much no palliative care
-# the rest varies
+# emergency.patients.per.week <- group_by(patient.admissions, Year.Week, Resource.Pool.name) %>%
+#                      filter(Method.of.Admission.Category == "Emergency Admission") %>%
+#                      tally() %>%
+#                      dcast(Year.Week ~ Resource.Pool.name)  %>%
+#                      setNames(c("Year.Week","elderly","medical","palliative","surgical","unscheduled","women.child")) %>%
+#                      all_na_to_0()
+# # pretty much no palliative care
+# # the rest varies
+# ggplot(emergency.patients.per.week, aes(Year.Week)) +
+#   geom_line(aes(y = emergency.patients.per.week$elderly, colour = "Elderly Care", group=1)) +
+#   geom_line(aes(y = emergency.patients.per.week$medical, colour = "Medical", group=1)) +
+#   geom_line(aes(y = emergency.patients.per.week$surgical, colour = "Surgical", group=1)) +
+#   geom_line(aes(y = emergency.patients.per.week$unscheduled, colour = "Unscheduled Care", group=1)) +
+#   geom_line(aes(y = emergency.patients.per.week$women.child, colour = "Women and Child", group=1)) +
+#   theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
+# 
+# acf(emergency.patients.per.week$elderly) #still nope
+# ggplot(emergency.patients.per.week, aes(Year.Week)) + 
+#   geom_line(aes(y = emergency.patients.per.week$elderly, colour = "Elderly Care", group=1)) + 
+#   theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
+# emergency.elderly.model <- arima(emergency.patients.per.week$elderly, order=c(1,0,1))
+# emergency.elderly.prediction <- predict(emergency.elderly.model, n.ahead=prediction.length, interval = 'confidence')
+# emergency.elderly.forecast <- forecast(emergency.elderly.model, level = c(95), h = prediction.length)
+# autoplot(emergency.elderly.forecast)
